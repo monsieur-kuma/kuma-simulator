278017eedde275d9b09f3bac1d272ca0dc308b40
Linh Nguyễn <vanlinh14121@gmail.com>
2024-06-30 01:08:44 +0700

    send request to extension

---
+++ src/lib/extensions.ts
@@ -0,0 +1,19 @@
+/* global chrome */
+const ID = 'jnjifnfegodjnliblglcbmmldgcienei';
+
+export const sendData = (action: string, data = {}) =>
+  new Promise((resolve) => {
+    if (chrome?.runtime?.sendMessage) {
+      chrome.runtime.sendMessage(
+        ID,
+        {
+          action,
+          data
+        },
+        (res) => {
+          resolve(res);
+        }
+      );
+    } else resolve("Chrome not found");
+  });
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.REACT_APP_STAGE === 'development' ? '' : '/HSR-Simulator/',
+  define: {
+    'import.meta.env.REACT_APP_STAGE': JSON.stringify(process.env.REACT_APP_STAGE),
+  },
 })
